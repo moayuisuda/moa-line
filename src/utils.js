4bb3bb511e7fa0ec44f4fa212a346ba53ac11e26
Your Name <you@example.com>
2020-01-15 12:43:47 +0800

    an

--- src/utils.js
+++ src/utils.js
@@ -14,26 +14,26 @@ const move = (
   after,
   fn = pro => {
     return Math.sqrt(pro, 2);
-  }
+  } // 缓动函数
 ) => {
-  if (fn(1) != 1) throw '[moaline-move] The fn must satisfy "fn (1) == 1"'; // 当参数为1时，对应的值也一定要为1
+  if (fn(1) != 1) throw '[moaline-move] The fn must satisfy "fn (1) == 1"';
 
   let st, sp;
-  st = performance.now(); // 保存开始时间
-  sp = copy(origin); // 保存起点
-  let d = {}; // 源与目标之间每一项的距离
+  st = performance.now();
+  sp = copy(origin);
+  let d = {};
   for (let i in origin) {
     d[i] = target[i] - origin[i];
   }
 
   let frame = t => {
-    let pro = (t - st) / duration; // 当前进程
+    let pro = (t - st) / duration;
     if (pro >= 1) {
       return;
     }
 
     for (let i in origin) {
-      origin[i] = sp[i] + fn(pro) * d[i]; // fn(pro)得出当前时间对应的缓动函数的距离百分比，再乘以总距离
+      origin[i] = sp[i] + fn(pro) * d[i];
     }
 
     if(after) after(copy(origin), pro);
